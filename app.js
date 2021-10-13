fc94ff8675185d64a297f3fb9bbcc15889798fdc
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-13 17:45:17 -0500

    fixed code that was causing error when typing in name of person that doesn't exisit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function mainMenu(person, people){
 
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
-  if(!person){
+  if(person.length === 0){
     alert("Could not find that individual.");
     return app(people); // restart
   }
